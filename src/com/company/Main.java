0f28d5178f92d5dccd411647d3f7cdaed570441b
Михеева Елена Валентиновна <mikheevaev1454@sao.obr.mos.ru>
2020-11-03 14:58:54 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        int a,b;
+        String oper;
+        a = scr.nextInt();
+        b = scr.nextInt();
+        oper = scr.next();
+
+
+
+        }
+    }
+
+
